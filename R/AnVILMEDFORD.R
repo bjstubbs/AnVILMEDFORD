9ab0f9577778ad23922bcb429d85524ee238c31e
BJ Stubbs <benjamin.stubbs@tufts.edu>
2024-09-08 20:12:43 -0400

    adding code for paper

--- R/AnVILMEDFORD.R
+++ R/AnVILMEDFORD.R
@@ -12,7 +12,6 @@
 anvilMedfordContributor<-function(docText,type="owner"){
      print(paste("@Contributor", docText))
      print(paste("@Contributor-Role", type))
-     print("")
  }
 
 #' Function to get and output workspace authors as MEDFORD
@@ -21,6 +20,8 @@ anvilMedfordContributor<-function(docText,type="owner"){
 #' @return MEDFORD text
 #' @export
 docOwners<-function(wsns,ws){
+  require(AnVIL)
+  terra=Terra()
   res=terra$getWorkspace(wsns,ws)
   resList=as.list(res)
   for(owner in resList$owners){
@@ -38,12 +39,24 @@ anvilMedfordWorkspace<-function(docText,minorTag=NA){
    }else{print(paste0("@Workspace ",docText))}
  }
 
+ #' Function to output AnVIL data MEDFORD
+ #' @return MEDFORD text
+ #' @export
+ anvilMedfordData<-function(docText,minorTag=NA){
+       if(!is.na(minorTag)){
+      print(paste0("@Data-", minorTag," ",docText))
+    }else{print(paste0("@Data ",docText))}
+  }
+
+
 #' Function to get and output workspace creator as MEDFORD
 #' @param wsns the workspace namespace of the workspace
 #' @param ws the workspace name of the workspace
 #' @return MEDFORD text
 #' @export
 docCreatedBy<-function(wsns,ws){
+  require(AnVIL)
+  terra=Terra()
   res=terra$getWorkspace(wsns,ws)
   resList=as.list(res)
   anvilMedfordContributor(resList$workspace$createdBy,type="creator")
@@ -54,12 +67,51 @@ docCreatedBy<-function(wsns,ws){
 #' @return MEDFORD text
 #' @export
 docWorkspace<-function(wsns,ws){
+  require(AnVIL)
+  terra=Terra()
   res=terra$getWorkspace(wsns,ws)
   resList=as.list(res)
   anvilMedfordWorkspace(ws)
   anvilMedfordWorkspace(ws,"workspace")
   anvilMedfordWorkspace(wsns,"namespace")
   anvilMedfordWorkspace(resList$workspace$billingAccount,"billingAccount")
+  anvilMedfordWorkspace(resList$workspace$bucketName,"bucketName")
+  anvilMedfordWorkspace(resList$workspace$createdBy,"createdBy")
+  anvilMedfordWorkspace(resList$workspace$createdDate,"createdDate")
+  anvilMedfordWorkspace(resList$workspace$lastModified,"lastModified")
+}
+
+
+#' Function to document  workspace Data details
+#' @return MEDFORD text
+#' @export
+docWorkspaceData<-function(wsns,ws){
+  require(AnVIL)
+  terra=Terra()
+  res=terra$getWorkspace(wsns,ws)
+  resList=as.list(res)
+  atts=resList$workspace$attributes
+  for(i in 1:length(atts)){
+    if(length(atts[[i]])==1){
+      anvilMedfordData(names(atts)[i])
+      anvilMedfordData("WorkspaceData","Role")
+      anvilMedfordData(resList$workspace$attributes[[i]],"Value")
+    }
+  }
+}
+
+#' Function to document workspace Entity details
+#' @return MEDFORD text
+#' @export
+docWorkspaceEntities<-function(wsns,ws){
+  require(AnVIL)
+  terra=Terra()
+  temp=avtables(wsns,ws)
+  for(i in 1:nrow(temp)){
+      anvilMedfordData(temp[i,1])
+      anvilMedfordData("Table","Role")
+      anvilMedfordData(temp[i,1],"Value")
+  }
 }
 
 #' Function to Extract csv file paths from medford bagit
@@ -75,6 +127,8 @@ bagItavTables<-function(zipFile){
 #' @param dataFile csv filename from BAGIT
 #' @return Boolean success
 uploadBagitAvtable<-function(wsns,ws,zipFile,dataFile){
+  require(AnVIL)
+  terra=Terra()
   temp=strsplit(dataFile,split="/")[[1]]
   temp=temp[length(temp)]
   obName=strsplit(temp,split=".",fixed=TRUE)
@@ -82,10 +136,3 @@ uploadBagitAvtable<-function(wsns,ws,zipFile,dataFile){
   dat=read.csv(unz(zipFile,dataFile))
   avtable_import(dat,entity=obName,name=ws,namespace=wsns)
 }
-
-#' Function to get broad ids from perturbagen name
-#' @param pertName the name of the perturbagen
-#' @param sigInfo sig file data
-#' @param phase which phase of data are we using
-#' @return a vector of broad ids
-#' @export
